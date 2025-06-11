857dadf3f831d3f2c3fa3f3f2a12a9428ca57303
helddc <projetogestaomil@gmail.com>
2025-06-10 22:53:13 -0300

    database.py

---
+++ database.py
@@ -0,0 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Transaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    txid = db.Column(db.String(100), unique=True, nullable=False)
+    status = db.Column(db.String(20), nullable=False, default='pending')
+    net_amount = db.Column(db.Float, nullable=False)
+    charge_amount = db.Column(db.Float, nullable=False)
+    payout_pix_key = db.Column(db.String(150), nullable=False)
+    payout_pix_key_type = db.Column(db.String(20), nullable=False, default='random')
