36228e0c8f02bbe64a1345035198cdec6ab32364
helddc <projetogestaomil@gmail.com>
2025-06-10 23:03:38 -0300

    config.py

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
 import os
 
+# Lê as chaves do ambiente seguro do Render
 ANUBIS_PUBLIC_KEY = os.getenv("ANUBIS_PUBLIC_KEY")
 ANUBIS_PRIVATE_KEY = os.getenv("ANUBIS_PRIVATE_KEY")
 
+# Regras de negócio
 FIXED_FEE = 2.50
 PERCENTAGE_COMMISSION = 0.15
 MINIMUM_VALUE = 20.00
 
+# API URL
 ANUBIS_API_URL = "https://api.anubis.gg/api"
